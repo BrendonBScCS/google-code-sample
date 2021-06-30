a799b7c75cd16ba1ced9fa5d082ef9b9d2f6e965
BrendonCurmi <brendoncrm@gmail.com>
2021-06-30 13:20:04 +0200

    Empty VideoPlaylist

--- java/src/main/java/com/google/VideoPlaylist.java
+++ java/src/main/java/com/google/VideoPlaylist.java
@@ -3,36 +3,4 @@ package com.google;
 /** A class used to represent a Playlist */
 class VideoPlaylist {
 
-    private Video playingVideo;
-    private boolean paused;
-
-    public Video getPlayingVideo() {
-        return playingVideo;
-    }
-
-    public void setPlayingVideo(Video video) {
-        playingVideo = video;
-        paused = false;
-    }
-
-    public boolean hasVideoPlaying() {
-        return playingVideo != null;
-    }
-
-    public boolean isVideoPlaying(Video video) {
-        return playingVideo == video;
-    }
-
-    public void stopVideo() {
-        setPlayingVideo(null);
-        paused = false;
-    }
-
-    public boolean isPaused() {
-        return paused;
-    }
-
-    public void setPaused(boolean paused) {
-        this.paused = paused;
-    }
 }
